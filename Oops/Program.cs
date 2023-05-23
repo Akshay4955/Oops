0505f89ae20f461a725db749a737f6484399cc63
Akshay4955 <akshayshedge11@gmail.com>
2023-05-23 20:59:35 +0530

    Add project files.

---
+++ Oops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Oops Problems Project");
+        }
+    }
+}
